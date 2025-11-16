042dd2a17bbdecb29324a8bef52a3f7a4ebfee21
pwberry <pb112233@gmail.com>
2025-11-16 09:09:21 -0500

    Update About.tsx
    deleted semicolon in apprentice card entry at end

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -32,7 +32,7 @@ const About = () => {
               image={apprenticeImage}
               imageAlt="Apprenticeship program participants"
               link="https://surface.syr.edu/join/"
-              external;
+              external
             />
             
             <FeatureCard
