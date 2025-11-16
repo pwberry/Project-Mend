9b75dc98596b344cdd10e76afbf8dfc9ef7058c2
pwberry <pb112233@gmail.com>
2025-11-16 11:04:04 -0500

    Update About.tsx
    edited news description

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -78,7 +78,7 @@ const About = () => {
             
             <FeatureCard
               title="News and Events"
-              description="Find out about upcoming news and events."
+              description="Learn about Project Mend’s upcoming workshops, public programs, and celebrations that highlight storytelling, digital media, and community collaboration."
               image={newsImage}
               imageAlt="Community gathering at event"
               link="/news"
