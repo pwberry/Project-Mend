780c3c4f5d2fb8c516219009fd89a0a8a3c213b3
pwberry <pb112233@gmail.com>
2025-11-16 10:05:12 -0500

    Update About.tsx
    italicize render

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -45,11 +45,11 @@ const About = () => {
             />
             
             <FeatureCard
-              title="Special Issue: render"
-              description="render, created by Writing and Rhetoric graduate Katherine Nikolau, celebrates the creative work and activism of nine justice-impacted artists."
-              imageAlt="Cover of render special issue"
-              link="https://surface.syr.edu/mendbooks/1/"
-              external
+            title={<span>Special Issue: <em>render</em></span>}
+            description="render, created by Writing and Rhetoric graduate Katherine Nikolau, celebrates the creative work and activism of nine justice-impacted artists."
+            imageAlt="Cover of render special issue"
+            link="https://surface.syr.edu/mendbooks/1/"
+            external
             />
 
             {/* Row 2: 2 cards */}
